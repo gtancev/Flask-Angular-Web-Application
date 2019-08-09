e02489f7a1e7d18918ab0db79893a163471239b9
Georgi Tancev <30585013+gtancev@users.noreply.github.com>
2019-08-09 10:38:57 +0200

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -8,17 +8,20 @@ PORT = 8081 #os.environ.get('PORT')
 # initialize flask application
 app = Flask(__name__)
 
+# read model
+clf = joblib.load('model.pkl').set_params(n_jobs=1)
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    # get iris object from request
+    
+    # get object from request
     X = request.get_json()
     X = [[float(X['postsittingsystolicbp']), float(X['a_g_ratio']), float(X['albumin']), 
         float(X['creatinine']),float(X['hemoglobin']), float(X['ldh_total']),
         float(X['lymphocytes']), float(X['rbc']),float(X['rdw']),
         float(X['spkt_v_total']),float(X['ufr']),float(X['z_scored_weight'])]]
 
-    # read model
-    clf = joblib.load('model.pkl').set_params(n_jobs=1)
+    # predict
     probabilities = clf.predict_proba(X)
 
     return jsonify([{'name': 'robust', 'value': round(probabilities[0, 0] * 100, 2)},
